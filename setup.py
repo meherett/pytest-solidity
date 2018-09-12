8d06aca7300cebfcca78ecb623edf739e045b678
Meheret <meherett@zoho.com>
2018-09-12 04:18:00 +0300

    Upgrade v0.0.2.dev1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name="pytest-cobra",
-    version='0.0.1',
-    description='pytest plugin for testing Ethereum Smart Contracts',
+    version='0.0.2.dev1',
+    description='PyTest plugin for testing Smart Contracts for Ethereum blockchain.',
     long_description='TODO',
     license='MIT',
     author='Meheret Tesfaye',
     author_email='meherett@zoho.com',
     url='https://github.com/Cobraframework/pytest-cobra',
-    python_requires='>=3.6',
+    python_requires='>=3.5,<3.7',
     packages=['pytest_cobra'],
     install_requires=[
         'pytest>=3.7.1,<4.0.0',
@@ -25,7 +25,9 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "Framework :: Pytest",
     ],
 )
