96611f43a698e7a842572a78183b948bdae548ca
Meheret <meherett@zoho.com>
2018-09-12 11:09:46 +0300

    update v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytest-cobra",
-    version='0.0.2.dev1',
+    version='0.0.2',
     description='PyTest plugin for testing Smart Contracts for Ethereum blockchain.',
     long_description='TODO',
     license='MIT',
