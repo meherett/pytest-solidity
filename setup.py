e3114666e665518f39cb6c6f759b7df6722d68e2
meherett <meherett@zoho.com>
2019-05-25 17:05:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
+import os
 
-with open("README.md", "r") as readme:
+base_dir = os.path.dirname(__file__)
+
+with open(os.path.join(base_dir, "README.md"), "r") as readme:
     long_description = readme.read()
 
 setup(
