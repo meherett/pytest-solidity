7cd10f6928d2c4271dd91ddeec4d8e6794ed74dd
meherett <meherett@zoho.com>
2020-08-13 11:44:36 +0300

    Added account.py

---
+++ pytest_eth/account.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from web3 import Web3
+from eth_typing import Address
+
+
+class Account(Address):
+    
+    def __new__(cls, web3: Web3, address: Address):
+        obj = super().__new__(cls, address)
+        obj.web3 = web3
+        obj.address = address
+        return obj
+
+    def transfer(self, address: Address, amount: int):
+        self.web3.eth.sendTransaction(transaction={
+            "to": address, "from": self.address, "value": amount
+        })
+
+    @property
+    def balance(self):
+        return self.web3.eth.getBalance(account=self.address)
